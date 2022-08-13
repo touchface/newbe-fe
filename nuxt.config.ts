0aad72b80fb2cc130cda1e1d1b8eb246d32ac332
songjiong <572165213@qq.com>
2022-08-14 03:00:44 +0800

    修复BUILD异常

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,69 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
+import IconsResolver from "unplugin-icons/resolver";
+import Components from "unplugin-vue-components/vite";
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+const lifecycle = process.env.npm_lifecycle_event;
+
 export default defineNuxtConfig({
-    vite: {
-        css: {
-          preprocessorOptions: {
-            scss: {
-            }
-          }
-        }
-      }
-})
+  // meta
+  meta: {
+    title: "NEWBE.PLUS",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "NEWBE.PLUS",
+        content: "NEWBE.PLUS",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+
+  // css
+  css: [],
+
+  // build
+  build: {
+    transpile: lifecycle === "build" ? ["element-plus"] : [],
+  },
+
+  // build modules
+  buildModules: [
+    "@pinia/nuxt",
+    "@nuxtjs/svg",
+    "@vueuse/nuxt",
+    "nuxt-windicss",
+    "unplugin-icons/nuxt",
+  ],
+
+  // auto import components
+  components: true,
+
+  // vite plugins
+  vite: {
+    plugins: [
+      Components({
+        dts: true,
+        resolvers: [IconsResolver({})],
+      }),
+    ],
+  },
+
+  // vueuse
+  vueuse: {
+    ssrHandlers: true,
+  },
+
+  // windicss
+  windicss: {
+    analyze: {
+      analysis: {
+        interpretUtilities: false,
+      },
+      server: {
+        port: 4000,
+        open: false,
+      },
+    },
+    scan: true,
+  },
+});
