1eda734a6b5be616da175e3643156ab0ed37247d
songjiong <572165213@qq.com>
2022-08-13 18:09:54 +0800

    项目初始化

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtPage/>
+</template>
+<style>
+@import 'element-plus/dist/index.css';
+html,body,#__nuxt{
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-container class="container">
+      <el-header></el-header>
+      <el-main class="main">
+            <div ><img style="width:400px;display: block;margin: 0 auto;margin-top: 200px;" src="/svg/newbe.svg"/></div>
+      </el-main>
+      <el-footer></el-footer>
+    </el-container>
+</template>
+<style lang="scss" scoped>
+.container {
+    height: 100%;
+}
+.main {
+    height: calc(100% - 120px);
+}
+</style>
